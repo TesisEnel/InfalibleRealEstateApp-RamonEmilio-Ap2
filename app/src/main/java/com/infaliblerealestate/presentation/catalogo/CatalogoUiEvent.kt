8a19b3789926639a9c7568643a1955059181521b
Danielvi <danielvilopez@gmail.com>
2025-11-24 20:33:07 -0400

    Agrega la interfaz para manejar los eventos de la Pantalla Catalogo

---
+++ app/src/main/java/com/infaliblerealestate/presentation/catalogo/CatalogoUiEvent.kt
@@ -0,0 +1,24 @@
+package com.infaliblerealestate.presentation.catalogo
+
+interface CatalogoUiEvent {
+    data object hideSheet: CatalogoUiEvent
+    data class loadPropiedad(val id: Int): CatalogoUiEvent
+    data object userMessageShown: CatalogoUiEvent
+    data object  hideFilterDialog: CatalogoUiEvent
+    data object showFilterDialog: CatalogoUiEvent
+    data object filterPropiedades: CatalogoUiEvent
+    data object clearFilters: CatalogoUiEvent
+    data class onFilterCasa(val filter: Boolean): CatalogoUiEvent
+    data class onFilterDepartamento(val filter: Boolean): CatalogoUiEvent
+    data class onFilterVilla(val filter: Boolean): CatalogoUiEvent
+    data class onFilterPenthouse(val filter: Boolean): CatalogoUiEvent
+    data class onFilterSolar(val filter: Boolean): CatalogoUiEvent
+    data class onFilterLocalComercial(val filter: Boolean): CatalogoUiEvent
+    data class onFilterPrecio(val precio: Float): CatalogoUiEvent
+    data class onFilterHabitaciones(val habitaciones: Int): CatalogoUiEvent
+    data class onFilterBanos(val banos: Int): CatalogoUiEvent
+    data class onFilterParqueos(val parqueos: Int): CatalogoUiEvent
+    data class onFilterAlquiler(val alquiler: Boolean): CatalogoUiEvent
+    data class onFilterVenta(val venta: Boolean): CatalogoUiEvent
+    data object showAllFilters: CatalogoUiEvent
+}
