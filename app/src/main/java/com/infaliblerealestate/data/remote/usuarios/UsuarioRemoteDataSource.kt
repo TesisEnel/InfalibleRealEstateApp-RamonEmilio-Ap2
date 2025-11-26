748f73da74c375540b13eb45093fcee76a9c7d8d
Danielvi <danielvilopez@gmail.com>
2025-11-26 18:01:23 -0400

    Agrega el remote data source del usuario para manejar mas llamadas al Api

---
+++ app/src/main/java/com/infaliblerealestate/data/remote/usuarios/UsuarioRemoteDataSource.kt
@@ -0,0 +1,53 @@
+package com.infaliblerealestate.data.remote.usuarios
+
+import com.infaliblerealestate.data.remote.Resource
+import com.infaliblerealestate.data.remote.dto.login.LoginRequest
+import com.infaliblerealestate.data.remote.dto.usuarios.UsuarioRequest
+import com.infaliblerealestate.data.remote.dto.usuarios.UsuarioResponse
+import javax.inject.Inject
+
+class UsuarioRemoteDataSource @Inject constructor(
+    private val apiService: UsuariosApiService
+) {
+    suspend fun validarCredenciales(credenciales: LoginRequest): Resource<UsuarioResponse?>{
+        return try {
+            val response = apiService.validarCredenciales(credenciales)
+            if (response.isSuccessful){
+                response.body().let{Resource.Success(it)}
+            }else{
+                return Resource.Error("Error ${response.code()}: ${response.message()}")
+            }
+        }catch (e: Exception){
+            Resource.Error("Error: ${e.localizedMessage} ?: Ocurrio un error al validar las credenciales")
+        }
+    }
+
+    suspend fun getUsuario(id: Int): Resource<UsuarioResponse?>{
+        return try {
+            val response = apiService.getUsuario(id)
+            if(response.isSuccessful){
+                response.body().let{Resource.Success(it)}
+
+            }else{
+                return Resource.Error("Error ${response.code()}: ${response.message()}")
+            }
+        }catch (e: Exception){
+            Resource.Error("Error ${e.localizedMessage} ?: Ocurrio un error al obtener el usuario")
+        }
+    }
+
+    suspend fun putUsuario(id: Int, usuario: UsuarioRequest): Resource<UsuarioResponse?>{
+        return try{
+            val response = apiService.putUsuario(id, usuario)
+            if(response.isSuccessful){
+                response.body().let{Resource.Success(it)}
+            }else{
+                return Resource.Error("Error ${response.code()}: ${response.message()}")
+            }
+        }catch (e: Exception){
+            Resource.Error("Error ${e.localizedMessage} ?: Ocurrio un error al actualizar el usuario")
+        }
+
+    }
+
+}
