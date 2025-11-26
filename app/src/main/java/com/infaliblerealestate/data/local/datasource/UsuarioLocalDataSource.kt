269e8a681976c9ecfeec276eaac8eb95e9dd990d
Danielvi <danielvilopez@gmail.com>
2025-11-26 17:50:34 -0400

    Agrega la clase UsuarioLocalDataSource para manejar operaciones locales de usuarios

---
+++ app/src/main/java/com/infaliblerealestate/data/local/datasource/UsuarioLocalDataSource.kt
@@ -0,0 +1,20 @@
+package com.infaliblerealestate.data.local.datasource
+
+import com.infaliblerealestate.data.local.dao.UsuarioDao
+import com.infaliblerealestate.data.local.entities.UsuarioEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UsuarioLocalDataSource @Inject constructor(
+    private val dao: UsuarioDao
+) {
+    fun getUsuario(id: String): Flow<UsuarioEntity?> = dao.getUsuario(id)
+
+    fun getUsuarioActual(): Flow<UsuarioEntity?> = dao.getUsuarioActual()
+
+    suspend fun insertUsuario(usuario: UsuarioEntity) = dao.insertUsuario(usuario)
+
+    suspend fun clearSession() = dao.deleteAllUsuarios()
+
+    suspend fun getLastSync(id: String): Long? = dao.getLastSync(id)
+}
