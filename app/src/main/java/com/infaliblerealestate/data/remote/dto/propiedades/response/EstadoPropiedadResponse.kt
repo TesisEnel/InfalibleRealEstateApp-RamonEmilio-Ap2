c9252b5adbbd90e96de36e7b3d33612e42bb5a66
Danielvi <danielvilopez@gmail.com>
2025-11-30 18:39:20 -0400

    Crea el DTO para la respuesta de Estado de Propiedad

---
+++ app/src/main/java/com/infaliblerealestate/data/remote/dto/propiedades/response/EstadoPropiedadResponse.kt
@@ -0,0 +1,7 @@
+package com.infaliblerealestate.data.remote.dto.propiedades.response
+
+data class EstadoPropiedadResponse (
+    val estadoPropiedadId: Int,
+    val nombreEstado: String?,
+    val descripcion: String?
+)
