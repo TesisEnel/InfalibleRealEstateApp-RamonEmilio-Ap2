3461c2e3f012443b0664b67d7ad66c815f9ef335
Danielvi <danielvilopez@gmail.com>
2025-11-30 18:28:44 -0400

    Refactoriza la barra de navegación inferior y mejora la interfaz de usuario

--- app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppBottomBar.kt
+++ app/src/main/java/com/infaliblerealestate/presentation/util/navigation/AppBottomBar.kt
@@ -1,5 +1,6 @@
 package com.infaliblerealestate.presentation.util.navigation
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +12,8 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,11 +25,12 @@ fun AppBottomBar(
     currentRoute: String?,
     usuarioId: String
 ) {
-
     NavigationBar(
         modifier = Modifier
             .height(70.dp)
+            .shadow(8.dp, RoundedCornerShape(50.dp))
             .clip(RoundedCornerShape(50.dp)),
+        containerColor = MaterialTheme.colorScheme.surface,
     ) {
         BottomNavItem.items.forEach { item ->
             val baseRoute = item.screen.route.substringBefore("/")
@@ -35,16 +39,21 @@ fun AppBottomBar(
             NavigationBarItem(
                 selected = selected,
                 onClick = {
-                    val route = when (item.screen) {
-                        is Screen.Home -> Screen.Home.createRoute(usuarioId)
-                        is Screen.Settings -> Screen.Settings.createRoute(usuarioId)
-                        is Screen.Carrito -> Screen.Carrito.createRoute(usuarioId)
-                        is Screen.Catalogo -> Screen.Catalogo.createRoute(usuarioId)
-                        else -> return@NavigationBarItem
-                    }
-                    navController.navigate(route) {
-                        popUpTo(Screen.Home.createRoute(usuarioId))
-                        launchSingleTop = true
+                    if (!selected) {
+                        val route = when (item.screen) {
+                            is Screen.Home -> Screen.Home.createRoute()
+                            is Screen.Settings -> Screen.Settings.createRoute(usuarioId)
+                            is Screen.Carrito -> Screen.Carrito.createRoute(usuarioId)
+                            is Screen.Catalogo -> Screen.Catalogo.createRoute()
+                            else -> return@NavigationBarItem
+                        }
+                        navController.navigate(route) {
+                            popUpTo(Screen.Home.route) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
                     }
                 },
                 icon = {
