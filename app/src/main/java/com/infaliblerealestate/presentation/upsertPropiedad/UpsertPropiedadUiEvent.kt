fca752a0e2be5f3a30c8f5a981dc4e3db4f700e2
Danielvi <danielvilopez@gmail.com>
2025-12-03 00:14:22 -0400

    feat: ✨Crea los eventos para eliminar una propiedad e imágenes

--- app/src/main/java/com/infaliblerealestate/presentation/upsertPropiedad/UpsertPropiedadUiEvent.kt
+++ app/src/main/java/com/infaliblerealestate/presentation/upsertPropiedad/UpsertPropiedadUiEvent.kt
@@ -1,10 +1,18 @@
 package com.infaliblerealestate.presentation.upsertPropiedad
 
+import android.net.Uri
+
 interface UpsertPropiedadUiEvent {
     data class LoadPropiedad(val id: Int) : UpsertPropiedadUiEvent
+    data class OnDeletePropiedad(val propiedadId: Int) : UpsertPropiedadUiEvent
+    data class OnDeleteServerImage(val imagenId: Int) : UpsertPropiedadUiEvent
+    data object ShowDeleteDialog : UpsertPropiedadUiEvent
+    data object HideDeleteDialog : UpsertPropiedadUiEvent
     data object UserMessageShown : UpsertPropiedadUiEvent
     data object SavePropiedad : UpsertPropiedadUiEvent
     data object ClearForm : UpsertPropiedadUiEvent
+    data class OnImagesSelected(val uris: List<Uri>) : UpsertPropiedadUiEvent
+    data class OnDeleteSelectedImage(val uri: Uri) : UpsertPropiedadUiEvent
     data class OnTituloChanged(val titulo: String) : UpsertPropiedadUiEvent
     data class OnPrecioChanged(val precio: Double) : UpsertPropiedadUiEvent
     data class OnMonedaChanged(val moneda: String) : UpsertPropiedadUiEvent
