685554ddd5bb669c24d3427d562dd4e1479f3337
Danielvi <danielvilopez@gmail.com>
2025-12-03 01:55:11 -0400

    feat: ✨Crea las funciones para actualizar y obtener un usuario por ID en el Dao de usuarios

--- app/src/main/java/com/infaliblerealestate/data/local/dao/UsuarioDao.kt
+++ app/src/main/java/com/infaliblerealestate/data/local/dao/UsuarioDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.infaliblerealestate.data.local.entities.UsuarioEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -18,9 +19,15 @@ interface UsuarioDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUsuario(usuario: UsuarioEntity)
 
+    @Update
+    suspend fun updateUsuario(usuario: UsuarioEntity)
+
     @Query("DELETE FROM usuarios")
     suspend fun deleteAllUsuarios()
 
     @Query("SELECT lastSync FROM usuarios WHERE id = :id")
     suspend fun getLastSync(id: String): Long?
+
+    @Query("SELECT * FROM usuarios WHERE id = :id")
+    suspend fun getUsuarioById(id: String): UsuarioEntity?
 }
