85e607649fdc3e979166679fbf9486cfc1cd8d4f
Danielvi <danielvilopez@gmail.com>
2025-12-02 23:25:57 -0400

    feat: ✨Agrega los siguientes metodos: -uploadImages para subir las imagenes de las propiedades -deleteImages para eliminar las imagenes de las propiedades -getUbicaiones para obtener la lista de ciudades y provincia para el formulario de creacion o edicion de las propiedades

--- app/src/main/java/com/infaliblerealestate/data/remote/propiedades/PropiedadesApiService.kt
+++ app/src/main/java/com/infaliblerealestate/data/remote/propiedades/PropiedadesApiService.kt
@@ -4,13 +4,18 @@ import com.infaliblerealestate.data.remote.dto.propiedades.request.PropiedadesRe
 import com.infaliblerealestate.data.remote.dto.propiedades.response.CategoriaResponse
 import com.infaliblerealestate.data.remote.dto.propiedades.response.EstadoPropiedadResponse
 import com.infaliblerealestate.data.remote.dto.propiedades.response.PropiedadesResponse
+import com.infaliblerealestate.data.remote.dto.propiedades.response.UbicacionResponse
+import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PropiedadesApiService {
     @GET("api/Propiedades")
@@ -27,5 +32,11 @@ interface PropiedadesApiService {
     suspend fun getCategorias(): Response<List<CategoriaResponse>>
     @GET("api/propiedades/estado-propiedades")
     suspend fun getEstadoPropiedades(): Response<List<EstadoPropiedadResponse>>
-
+    @Multipart
+    @POST("api/Imagenes/upload/{propiedadId}")
+    suspend fun uploadImages(@Path("propiedadId") propiedadId: Int, @Part files: List<MultipartBody.Part>): Response<Unit>
+    @DELETE("api/Imagenes/Delete")
+    suspend fun deleteImages(@Query("imagenesIds") imagenesIds: List<Int>): Response<Unit>
+    @GET("api/Propiedades/ubicaciones")
+    suspend fun getUbicaciones(): Response<UbicacionResponse>
 }
